0333b2e0bac43734fca5c6f7b670899dba7c3c6f
Дударев Михаил <Palmer656@yandex.ru>
2024-05-25 10:58:28 +0300

    Зарегистрировал эндпоинты для отзывов и комментариев

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,26 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import CategoryViewSet, GenreViewSet
+from .views import (
+    CategoryViewSet,
+    GenreViewSet,
+    ReviewViewSet,
+    CommentViewSet,
+)
 
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'genres', GenreViewSet)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='review'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment',
+)
+
 
 urlpatterns = [
     path('v1/', include(router.urls)),
