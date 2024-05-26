7ee6fb1e12e86a28688f09856f7ac30a7ab68ad1
Victor Ryabov <victorwillhard@gmail.com>
2024-05-26 16:09:11 +0300

    feat: Add UserViewSet with CRUD operations and search functionality

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,12 +1,16 @@
 from django.utils.crypto import get_random_string
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework.filters import SearchFilter
 
 from users.models import ConfirmationCode, User
-from users.serializers import UserSerializer, ObtainTokenSerializer
+from users.serializers import UserSerializer, UserSignupSerializer, ObtainTokenSerializer
 from users.utils import send_mail
+from users.permissions import IsAdmin
 
 
 @api_view(['POST'])
@@ -14,7 +18,7 @@ def signup(request):
     """
     Регистрация пользователя.
     """
-    serializer = UserSerializer(data=request.data)
+    serializer = UserSignupSerializer(data=request.data)
     if serializer.is_valid():
         username = serializer.validated_data.get('username')
         email = serializer.validated_data.get('email')
@@ -47,3 +51,24 @@ def token(request):
         token = AccessToken.for_user(user)
         return Response({'token': str(token)}, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all().order_by('id')
+    lookup_field = 'username'
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
+    filter_backends = [SearchFilter]
+    search_fields = ['username']
+    http_method_names = ['get', 'post', 'delete', 'patch']
+
+    @action(detail=False, methods=['get', 'patch'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        if request.method == 'GET':
+            serializer = self.get_serializer(request.user)
+            return Response(serializer.data)
+        serializer = self.get_serializer(request.user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data['role'] = request.user.role
+        serializer.save()
+        return Response(serializer.data)
