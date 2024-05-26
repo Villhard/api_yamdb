8bc54f3fc17bb647457eb29de72f1fd662817f7a
Victor Ryabov <victorwillhard@gmail.com>
2024-05-26 16:08:25 +0300

    chore: Add user endpoints to router in users/urls.py

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from users import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('auth/signup/', views.signup, name='signup'),
     path('auth/token/', views.token, name='token'),
+    path('', include(router.urls)),
 ]
