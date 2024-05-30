40bbdfc3845b5f5d9fe36c27b94cc3f2002785ea
Дударев Михаил <Palmer656@yandex.ru>
2024-05-30 21:26:15 +0300

    Присвоить версию роутеру API

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -10,21 +10,24 @@ from .views import (
 )
 
 
-# TODO: Так как у нас может быть много версий API,
-#  то нужно в переменной обозначить к какой версии относится роутер
-router = routers.DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'genres', GenreViewSet)
-router.register(r'titles', TitleViewSet)
-router.register(
+router_v1 = routers.DefaultRouter()
+router_v1.register(r'categories', CategoryViewSet)
+router_v1.register(r'genres', GenreViewSet)
+router_v1.register(r'titles', TitleViewSet)
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='review'
 )
-router.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comment',
 )
 
+v1_urls = [
+    path('', include('users.urls')),
+    path('', include(router_v1.urls)),
+]
+
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(v1_urls)),
 ]
